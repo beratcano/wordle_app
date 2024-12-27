fa0549530e55f19eb9c117d4d27a05dbf98d6ea7
beratcano <beratcanozkan@ogr.iu.edu.tr>
2024-12-27 04:46:09 +0100

    staticfiles?

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,25 @@ use axum::{routing::{get, post}, Router};
 use std::sync::{Arc, Mutex};
 use crate::routes::admin::SharedAdminResult;
 use std::net::SocketAddr;
+use tower_http::services::ServeDir;
+
+use routes::staticfiles::static_routes;
 mod routes;
 mod models;
 
 #[tokio::main]
 async fn main() {
     let admin_result: SharedAdminResult = Arc::new(Mutex::new(None));
+
     let admin_routes = routes::admin::admin_routes(admin_result.clone());
     let user_routes = routes::user::user_routes(admin_result.clone());
+    let static_routes = static_routes(admin_result.clone());
+
     let app = Router::new()
     .route("/", get(routes::home::home))
     .merge(admin_routes)
-    .merge(user_routes);
+    .merge(user_routes)
+    .merge(static_routes);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Listenin on http://{}", addr);
--- src/routes/home.rs
+++ src/routes/home.rs
@@ -1,3 +1,7 @@
-pub async fn home() -> &'static str {
-    "Welcome to the Wordle Replica!"
+use axum::response::{IntoResponse, Html};
+use tower_http::services::ServeDir;
+use tower_service::Service;
+
+pub async fn home() -> impl IntoResponse {
+    Html(include_str!("../static/index.html"))
 }
